68f4004098feecb4affa491fd76ad98a65363945
Joyce <vieira.joyce@academico.ifpb.edu.br>
2025-04-15 10:12:55 -0300

    Texto que representa as mudanças realizadas

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <header style={{ backgroundColor: '#000',alignItems:"center", columnGap:"100px", display: 'flex', justifyContent: 'flex-start' }}>
+        <h1>Gestão de Projetos</h1>
+      </header>
         {children}
       </body>
     </html>
